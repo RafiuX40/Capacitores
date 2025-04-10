cddef6f39e4958d0214c53bf1363e2e0ec54a749
Rafael de Jesús Ramos Chávez <22090@portalcedi.edu.mx>
2025-04-10 09:10:56 -0600

    Implementacion de los 3 plugins

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton } from '@ionic/angular/standalone';
 import {Camera, CameraResultType} from '@capacitor/camera';
+import { LocalNotifications } from '@capacitor/local-notifications';
+import { Geolocation } from '@capacitor/geolocation';
 
 @Component({
   selector: 'app-home',
@@ -19,4 +21,52 @@ export class HomePage {
     });
     console.log(image.dataUrl);
   };
+
+  
+  async scheduleNotification(title: string, body: string) {
+    const permission = await LocalNotifications.requestPermissions();
+    if (permission.display !== 'granted') {
+      console.warn('Permisos de notificación no concedidos');
+      return;
+    }
+  
+    await LocalNotifications.schedule({
+      notifications: [
+        {
+          title,
+          body,
+          id: Date.now(), // ID único basado en timestamp
+          schedule: { at: new Date(Date.now() + 1000) }, // 1 segundo después
+          sound: 'beep.aiff',
+          smallIcon: 'ic_stat_icon_config_sample',
+          actionTypeId: '',
+        },
+      ],
+    });
+  }
+
+  async lanzarNotificacion() {
+    await this.scheduleNotification('Recordatorio', 'Tienes una tutoría en 10 minutos!');
+  }
+
+
+  async getCurrentPosition() {
+    try {
+      const permission = await Geolocation.requestPermissions();
+      if (permission.location !== 'granted') {
+        console.warn('Permiso de ubicación no concedido');
+        return null;
+      }
+  
+      const coordinates = await Geolocation.getCurrentPosition();
+      return {
+        lat: coordinates.coords.latitude,
+        lng: coordinates.coords.longitude
+      };
+    } catch (error) {
+      console.error('Error obteniendo la ubicación:', error);
+      return null;
+    }
+  }
+
 }
