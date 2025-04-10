28e355744169d9c45d905f3961f8b7ca637bc898
Rafael de Jesús Ramos Chávez <22090@portalcedi.edu.mx>
2025-04-10 08:40:55 -0600

    FUnciona todo

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton } from '@ionic/angular/standalone';
+import {Camera, CameraResultType} from '@capacitor/camera';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton],
 })
 export class HomePage {
   constructor() {}
+
+  takePhoto =async () => {
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: false,
+      resultType: CameraResultType.Uri,
+    });
+    console.log(image.dataUrl);
+  };
 }
